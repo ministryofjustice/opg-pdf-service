a5de8e75ca88471d4d4979201f53dddda6bfe26f
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-10-06 15:32:28 +0100

    Test changes on coverage to allow build
    (revert configuration in future PR)

--- src/lib/htmlToPdf.test.js
+++ src/lib/htmlToPdf.test.js
@@ -5,7 +5,7 @@ afterAll(async () => {
 });
 
 describe('Given you pass HTML to be returned into PDF', () => {
-  const testHtml = `<html><head></head><body><p><a href="/home" class="govuk-link">Test with no links</a></p></body></html>`;
+  const testHtml = `<html><head><script src="http://localhost/nonexistantfile"></script></head><body><p><a href="/home" class="govuk-link">Test with no links</a></p></body></html>`;
 
   describe('Given a PDF is not generated correctly due to an error', () => {
     test('it should return null and throw an error', () => {
@@ -22,7 +22,7 @@ describe('Given you pass HTML to be returned into PDF', () => {
   describe('Given a PDF is valid and to be generated', () => {
     test('it should generate a PDF using puppeteer successfully', async () => {
       const pdf = await htmlToPdf(testHtml, {
-        waitUntil: 'load',
+        waitUntil: 'networkidle0',
         pageHeight: 2000,
         pageWidth: 1100,
       });
@@ -35,7 +35,7 @@ describe('Given you pass HTML to be returned into PDF', () => {
   describe('Given a PDF is valid with page sizing sent and to be generated', () => {
     test('it should generate a PDF using puppeteer successfully', async () => {
       const pdf = await htmlToPdf(testHtml, {
-        waitUntil: 'load',
+        waitUntil: 'networkidle0',
         pageHeight: 20,
         pageWidth: 40,
       });
