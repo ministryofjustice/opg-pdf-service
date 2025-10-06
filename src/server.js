41fc14394d2a83077289db57db7f5f611c242365
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-10-06 14:49:04 +0100

    Initialise browser component as a part of startup.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 /* istanbul ignore file */
 
 import app from './app.js';
+import { initBrowser } from './lib/htmlToPdf.js';
 
 const port = process.env.PDF_SERVICE_PORT || 80;
+
+await initBrowser();
 
 const server = app.listen(port, (err) => {
   if (err) throw err;
