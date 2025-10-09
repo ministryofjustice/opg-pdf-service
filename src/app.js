8a47e36c54104423162edc6c11a0f9123f073941
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-10-09 12:48:20 +0100

    Unused import

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import bodyParser from 'body-parser';
 import polka from 'polka';
 import GeneratePdf from './lib/generatePdf.js';
 import pdfService from './lib/pdfConfigService.js';
-import embedRemoteImages from './lib/embedRemoteImages.js';
 
 const app = polka()
   .use(bodyParser.text({ type: 'text/html', limit: '2000kb' }))
