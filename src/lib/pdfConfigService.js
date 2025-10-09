ddbf4ed88f1ff62c0dab8087c60938e654b0e11d
Adam Cooper <adam.cooper@publicguardian.gov.uk>
2025-10-09 12:10:55 +0100

    Pull embed remote images configuration from headers

--- src/lib/pdfConfigService.js
+++ src/lib/pdfConfigService.js
@@ -3,10 +3,15 @@ class PdfConfigService {
     value = parseInt(value);
     return Number.isInteger(value) ? value : 0;
   }
+
   static stripAnchorTagsFromHtml(value) {
     return value === 'true';
   }
 
+  static embedRemoteImages(value) {
+    return value === 'true';
+  }
+  
   static title(title) {
     return title ? title : 'View LPA - View a lasting power of attorney';
   }
